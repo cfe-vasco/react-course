0b4693f2a33f71e4995de2d402937676d1da94e7
CHRISTOPHER FERNANDO VASCO ESPIN <cfe.vasco@yavirac.edu.ec>
2023-05-15 13:16:00 -0500

    Operador condicional ternario

---
+++ 02-intro-javascript/src/bases/12-operadores-ternarios.js
@@ -0,0 +1,16 @@
+const active = true;
+
+// let message = '';
+
+// if(!active){
+//     message = 'usuario activo'
+// } else {
+//     message = 'usuario inactivo'
+// }
+
+// const message = (active) ? 'Activo' : 'Inactivo'
+// const message = (active) ? 'Activo' : null;
+const message = active && 'Activo'
+
+
+console.log(message);

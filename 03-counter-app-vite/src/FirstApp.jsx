649895f30e7014960a4844affff22f595e89a31e
CHRISTOPHER FERNANDO VASCO ESPIN <cfe.vasco@yavirac.edu.ec>
2023-05-15 16:33:10 -0500

    Impresión de variables en el HTML

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,8 +1,18 @@
+// const user = {
+//   name: 'Christopher Vasco',
+//   age: 22,
+// }
+
+const getPass = (edad) => {
+  return (edad >= 18) ? 'Puedes entrar a la discoteca' : 'Eres menor de edad'
+}
+
 export const FirstApp = () => {
   return (
     <>
-      <h1>Christopher</h1>
-      <p>Soy un subtitulo</p>
+      <h1>Bienvenido</h1>
+      <p>{getPass(19)}</p>
+      {/* <code>JSON.stringify{user}</code> */}
     </>
   );
 };

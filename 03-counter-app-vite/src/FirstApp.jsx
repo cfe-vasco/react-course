ca148cff0b4d569108cafca4ebc76769b4f30272
CHRISTOPHER FERNANDO VASCO ESPIN <cfe.vasco@yavirac.edu.ec>
2023-05-19 14:02:37 -0500

    Comunicación entre componentes - Props

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -3,16 +3,25 @@
 //   age: 22,
 // }
 
-const getPass = (edad) => {
-  return (edad >= 18) ? 'Puedes entrar a la discoteca' : 'Eres menor de edad'
-}
+// const getPass = (edad) => {
+//   return (edad >= 18) ? 'Puedes entrar a la discoteca' : 'Eres menor de edad'
+// }
 
-export const FirstApp = () => {
+// export const FirstApp = () => {
+//   return (
+//     <>
+//       <h1>Bienvenido</h1>
+//       <p>{getPass(19)}</p>
+//       {/* <code>JSON.stringify{user}</code> */}
+//     </>
+//   );
+// };
+
+export const FirstApp = ({title, subTitle}) => {
   return (
     <>
-      <h1>Bienvenido</h1>
-      <p>{getPass(19)}</p>
-      {/* <code>JSON.stringify{user}</code> */}
-    </>
+      <h1>{title}</h1>
+      <p>Soy un subtitulo: {subTitle + 1}</p>
+    </> 
   );
 };

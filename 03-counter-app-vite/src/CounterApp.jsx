cea259f924d1b8d12777c5fcbc7f6aeae4dab450
CHRISTOPHER FERNANDO VASCO ESPIN <cfe.vasco@yavirac.edu.ec>
2023-05-23 15:30:43 -0500

    Boton de remove y reset

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,22 +1,26 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 export const CounterApp = ({ value }) => {
-  
-  const [counter, setCounter ] = useState( value )
 
-  const handleAdd = () => {
-    setCounter( counter + 1 ) // ===> Toma el valor del counter y lo aumenta en 1
+  const [counter, setCounter] = useState(value);
+
+  const add = () => {
+    setCounter(counter + 1); // ===> Toma el valor del counter y lo aumenta en 1
     // setCounter ( (c) => c + 1 ) ===> esto es una funcion que regresa el nuevo valor del counter
-  }
+  };
+
+  const remove = () => setCounter(counter - 1)
+
+  const reset = () => setCounter (value)
 
   return (
     <>
       <h1>CounterApp</h1>
       <h2>{counter}</h2>
-      <button onClick={ handleAdd }>
-        +1
-      </button>
+      <button onClick={add}>+1</button>
+      <button onClick={remove}>-1</button>
+      <button onClick={reset}>Reset</button>
     </>
   );
 };

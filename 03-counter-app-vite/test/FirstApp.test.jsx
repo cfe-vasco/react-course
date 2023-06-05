a0aded22f20a1e72c7801fc7765ade787ac9e4f4
CHRISTOPHER FERNANDO VASCO ESPIN <cfe.vasco@yavirac.edu.ec>
2023-06-05 15:47:53 -0500

    Probar FirstApp

--- 03-counter-app-vite/test/FirstApp.test.jsx
+++ 03-counter-app-vite/test/FirstApp.test.jsx
@@ -4,9 +4,19 @@ import { FirstApp } from "../src/FirstApp"
 describe('Pruebas en FirstApp', () => {
     test('Debe de hacer match con snapshot', () => {
 
-        const subTittle = 'Esto es un subtitulo desde tests'
+        const subTitle = 'Esto es un subtitulo desde tests'
+        const {container} = render(<FirstApp subTitle={subTitle}/>)
 
-        render(<FirstApp subTitle={subTittle}/>)
+        expect(container).toMatchSnapshot()
 
+    })
+
+    test('Debe de mostrar el titulo en h1', () => {
+        const subTitle = 'Esto es un subtitulo desde tests 2'
+        const {container, getByText} = render(<FirstApp subTitle={subTitle}/>)
+        expect(getByText(subTitle) ).toBeTruthy()
+
+        const h1 = container.querySelector('p')
+        expect(h1.innerHTML).toContain(subTitle)
     })
 })

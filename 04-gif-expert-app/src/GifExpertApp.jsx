03c10193b29e031666bf4073fa93b6a307f8cda3
CHRISTOPHER FERNANDO VASCO ESPIN <cfe.vasco@yavirac.edu.ec>
2023-06-09 15:00:13 -0500

    Agregar una nueva categoría

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -2,6 +2,21 @@ import { useState } from 'react';
 
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+
+  const onAddCategory = () => {
+    // const newCategory = 'Valorant'
+
+    // if(categories.includes(newCategory)){
+    //     console.log('Esta categoria ya existe');
+    // } else {
+    //     setCategories([...categories, newCategory])
+    // }
+
+    // setCategories(newCategory => [...newCategory, 'Valorant'])
+
+    setCategories([...categories, 'Valorant'])
+
+  }
   console.log(categories);
 
   return (
@@ -12,6 +27,7 @@ export const GifExpertApp = () => {
       {/* Input */}
 
       {/* Listado de Gifs */}
+      <button onClick={onAddCategory}>Agregar</button>
       <ol>
         {categories.map((category) => {
           return <li key={category}>{category}</li>;

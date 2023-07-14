28d716a7d27cb39962ce4b20c4ba607f72e193c1
CHRISTOPHER FERNANDO VASCO ESPIN <cfe.vasco@yavirac.edu.ec>
2023-07-14 15:28:33 -0500

    Pruebas del componente - GifGridItem

--- 04-gif-expert-app/test/components/GifItem.test.jsx
+++ 04-gif-expert-app/test/components/GifItem.test.jsx
@@ -1,15 +1,27 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { GifItem } from '../../src/components/GifItem';
 
 describe('Pruebas en <GifItem/>', () => {
-    
   const title = 'Saitama';
-  const url = 'https://hola.com';
+  const url = 'https://hola.com/';
 
   test('Debe hacer match con el snapshot', () => {
-
     const { container } = render(<GifItem title={title} url={url} />);
-
     expect(container).toMatchSnapshot();
   });
+
+  test('Debe de mostrar la imagen con el URL y el ALT indicado', () => {
+    render(<GifItem title={title} url={url} />);
+
+    // expect(screen.getByRole('img').alt).toBe(title)
+
+    const {src, alt} = screen.getByRole('img');
+    expect(src).toBe(url) 
+    expect(alt).toBe(alt) 
+  });
+
+  test('Debe de mostrar el titulo en el componente', () => {
+    render(<GifItem title={title} url={url} />);
+    expect(screen.getByText(title)).toBeTruthy();
+  })
 });
